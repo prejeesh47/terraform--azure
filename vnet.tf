3b44c2f46c0099a2482c5ff773537619178f2dcd
prejeesh47 <prejeeshrajan47@gmail.com>
2023-03-10 11:44:07 +0530

    vnet resource update

---
+++ vnet.tf
@@ -0,0 +1,24 @@
+#vnet 
+resource "azurerm_virtual_network" "web-vnet" {
+  name                = "web-network"
+  location            = azurerm_resource_group.web-rg.location
+  resource_group_name = azurerm_resource_group.web-rg.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    env  = "dev"
+    tier = "frontendapp"
+
+  }
+  resource "azurerm_subnet" "web-subnet" {
+    name                 = "web-subnet"
+    resource_group_name  = azurerm_resource_group.web-rg.name
+    virtual_network_name = azurerm_virtual_network.web-vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
+
+    tags = {
+      env  = "dev"
+      tier = "frontendapp"
+
+    }
+  }
+}
