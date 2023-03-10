b65183236c4a922978f04673ba2c9698a574b734
prejeesh47 <prejeeshrajan47@gmail.com>
2023-03-10 17:29:01 +0530

    nsg rules for ssh & http

--- vnet.tf
+++ vnet.tf
@@ -11,13 +11,15 @@ resource "azurerm_virtual_network" "web-vnet" {
   }
 }
 
-
+#subnet
 resource "azurerm_subnet" "web-subnet" {
   name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.web-rg.name
   virtual_network_name = azurerm_virtual_network.web-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+#nsg
 resource "azurerm_network_security_group" "web-nsg" {
   name                = "web-rules"
   location            = azurerm_resource_group.web-rg.location
@@ -26,4 +28,32 @@ resource "azurerm_network_security_group" "web-nsg" {
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_network_security_rule" "web-ssh" {
+  name                        = "web-ssh"
+  priority                    = 500
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.web-rg.name
+  network_security_group_name = azurerm_network_security_group.web-nsg.name
+}
+
+resource "azurerm_network_security_rule" "web-http" {
+  name                        = "web-http"
+  priority                    = 400
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.web-rg.name
+  network_security_group_name = azurerm_network_security_group.web-nsg.name
 }
