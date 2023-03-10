707d2bf0ef254ac78a4caf00c6d19072a040ca23
prejeesh47 <prejeeshrajan47@gmail.com>
2023-03-10 17:15:44 +0530

    nsg

--- vnet.tf
+++ vnet.tf
@@ -18,3 +18,12 @@ resource "azurerm_subnet" "web-subnet" {
   virtual_network_name = azurerm_virtual_network.web-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_network_security_group" "web-nsg" {
+  name                = "web-rules"
+  location            = azurerm_resource_group.web-rg.location
+  resource_group_name = azurerm_resource_group.web-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
